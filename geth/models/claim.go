46c147322781ecce7f0b08be6c95da82842b602e
Dan Dapp <d.degterin@gmail.com>
2018-10-23 20:01:55 +0100

    delete old files

--- geth/models/claim.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type ClaimDeployParams struct {
-    BankAddress string `json:"bankAddress"`
-    ClaimMultimple bool `json:"claimMultimple"`
-}
-
--- geth/models/storage.go
+++
@@ -1,5 +0,0 @@
-package models
-
-type StorageDeployParams struct {
-    ConfigAddress string `json:"configAddress"`
-}
